d3ed39cf4d0e609fc28eaa606dd955bfe71ccf7d
Alex <alexmore080@gmail.com>
2022-08-15 21:22:52 -0500

    gif frame added for random quote

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var quoteEl = document.getElementById("quoteTextEl");
 var button = document.querySelector("btn");
 var quoteText = {};
+let gif = document.querySelector("#gif")
+
 
 var renderQuote = function () {
   var options = {
@@ -20,9 +22,38 @@ var renderQuote = function () {
       var attribution = document.createElement("h3");
       attribution.classList.add("place-content-end");
       attribution.textContent = " -" + data.Speaker;
+      console.log(data.Speaker)
       quoteEl.append(attribution);
+      handleRandomGif(data.speaker)
     })
     .catch((err) => console.error(err));
 };
+
+
+
+
+function handleRandomGif(speaker){
+  
+  let giphyAPI = "https://api.giphy.com/v1/gifs/random?api_key=tgs71BTlLJyX02wBBfoRfToFipaRcp8R&tag=" + speaker + "&rating=g"
+  fetch(giphyAPI).then(function (response){
+    if(response.ok){
+      response.json().then(function (data){
+        return data;
+      })
+      .then(function(data){
+        console.log(data);
+        let randomGif = document.createElement("iframe");
+        console.log(data.bitly_gif_url);
+        randomGif.setAttribute("src", data.data.embed_url);
+        console.log(randomGif)
+        gif.appendChild(randomGif);
+      })
+    }
+  })
+
+  
+
+
+}
 
 renderQuote();
